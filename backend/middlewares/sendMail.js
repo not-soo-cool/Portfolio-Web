283658dfd9c7056ca2db2c0617b28922dbf78d9b
not-soo-cool <yashvendrasengar1111@gmail.com>
2023-12-02 23:43:30 +0530

    Second commit

---
+++ backend/middlewares/sendMail.js
@@ -0,0 +1,20 @@
+import { createTransport } from "nodemailer";
+
+export const sendMail = async (options) => {
+  const transporter = createTransport({
+    host: process.env.SMPT_HOST,
+    port: process.env.SMPT_PORT,
+    service: process.env.SMPT_SERVICE,
+    auth: {
+      user: process.env.SMPT_MAIL,
+      pass: process.env.SMPT_PASSWORD,
+    },
+  });
+
+  await transporter.sendMail({
+    subject: "CONTACT REQUEST FROM PORTFOLIO",
+    to: process.env.SMPT_MAIL,
+    from: options.email,
+    text: options.userMessage,
+  });
+};
