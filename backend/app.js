bce417cd5431fd69c3b99dc0d431beb7b99b8463
not-soo-cool <yashvendrasengar1111@gmail.com>
2023-12-04 03:37:59 +0530

    Cors added

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,14 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 export const app = express();
+import cors from 'cors'
+
+const corsOptions = {
+    origin: 'https://portfolio-web-2rmk.onrender.com/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
+  
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -11,6 +19,7 @@ import { userRouter } from "./routes/User.js";
 app.use("/api/v1", userRouter);
 
 app.use(express.static(path.resolve("./frontend/build")));
+
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve("./frontend/build/index.html"));
